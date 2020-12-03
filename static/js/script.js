947baa1c732dfe36c5c083da41b560892ccf82f1
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2020-12-03 13:41:46 +0100

    Add materlize navbar and sidenav

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".sidenav").sidenav({edge: "right"});
+});
+
+
+/*
+    vanilla JavaScript for MaterializeCSS initialization
+*/
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let sidenavs = document.querySelectorAll(".sidenav");
+//     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+// });
